b21adbff57f5bb150fce7e43516eea4f5a2f25d2
Kiran Shahi <shahikiran575@gmail.com>
2025-01-15 09:23:01 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 12345
+let accountemail = "kiran@gmail.com"
+var accountpassword = "54321"
+accountcity = "surkhet"
+
+
+// accountId =2
+
+accountemail = "thakuri@gmail.com"
+accountpassword = "2468"
+accountcity = "gutu"
+
+
+console.log(accountId);
+
+// prefer not to use var
+
+console.table([accountId, accountemail, accountpassword, accountcity])
