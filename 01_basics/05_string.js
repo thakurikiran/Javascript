d54b3e67b6c91be5b73bd7368ff7d70b4d3d4755
Kiran Shahi <shahikiran575@gmail.com>
2025-04-11 01:57:04 +0000

    some chnges

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
+const name = "kiran"
 const repoCount = 50
 
 // console.log(name + repoCount + " Value");
 
 // console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('kiran-ks-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
