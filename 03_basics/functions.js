34026efe47f29d28f6ff7dff76cdc66b8c253948
Kiran Shahi <shahikiran575@gmail.com>
2025-04-12 19:36:53 +0000

    functions

---
+++ 03_basics/functions.js
@@ -0,0 +1,43 @@
+
+// function sayMYname() {
+//         console.log("k");
+//         console.log("I");
+//         console.log("R");
+//         console.log("A");
+//         console.log("N");
+
+// }
+
+//     sayMYname()
+
+
+    function addTwoNumbers(num1 ,num2){
+
+        // let result = num1+num2
+        // return result
+         return num1 + num2 
+         console.log("kiran"); //not  execute because of return 
+           
+    }
+   const result= addTwoNumbers(3,4)
+//    console.log("result:",result);
+
+
+   function loginUserMessage(username="kiran"){
+    if(username===undefined){
+        console.log("please enter your name");
+        return
+        
+    }
+     return `${username} just logged in`
+
+   }
+// console.log(loginUserMessage("Kiran"))
+
+console.log(loginUserMessage("shahi"));
+
+   
+   
+   
+
+
