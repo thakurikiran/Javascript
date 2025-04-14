ffe4bc90bbb051a9325ae3ab1790cca89f661252
Kiran Shahi <shahikiran575@gmail.com>
2025-04-14 18:55:04 +0000

    function and scopes

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -11,30 +11,68 @@
 //     sayMYname()
 
 
-    function addTwoNumbers(num1 ,num2){
+    function addTwoNumbers(num1 ,num2,num3){
 
         // let result = num1+num2
         // return result
-         return num1 + num2 
+         return num1 + num2 +num3
          console.log("kiran"); //not  execute because of return 
            
     }
-   const result= addTwoNumbers(3,4)
+   const result= addTwoNumbers(3,4,5)
 //    console.log("result:",result);
 
 
-   function loginUserMessage(username="kiran"){
+   function loginUserMessage(username){
     if(username===undefined){
-        console.log("please enter your name");
+        // console.log("please enter your name");
         return
         
     }
      return `${username} just logged in`
 
    }
-// console.log(loginUserMessage("Kiran"))
+// console.log(loginUserMessage())
 
-console.log(loginUserMessage("shahi"));
+// console.log(loginUserMessage("shahi"));
+
+function calculateCartPrice(val1,val2,...num1) {
+
+    return  num1
+}
+
+    // console.log(calculateCartPrice(100,200,300,400))
+
+   const user ={
+    username:"kiran",
+    email:"shahikirn4@gmail.com",
+    price:199
+   } 
+
+      function handleObject(jnyobject){
+        console.log(`username is ${jnyobject.username} and price is ${jnyobject.price}`);
+        
+    
+       }
+//  handleObject(user);
+
+handleObject({
+    username:"shahi",
+    price:500
+})
+
+
+const myNewArray =[200,399,488,577]
+
+function returnsecondval(arraydey){
+     return arraydey[1]
+    }
+
+console.log(returnsecondval([1,2,3,4]))
+
+
+
+  
 
    
    
---
+++ 03_basics/scopes.js
@@ -0,0 +1,59 @@
+
+// var c= 300
+let a = 200
+
+if (true){
+    let a = 20
+    const b = 30
+    // console.log("Inner :",a);
+    
+
+}
+// for (let i = 0; i <Array.length; i++){
+//     const element = array[i];
+// }
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "Kiran"
+    
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+    // console.log(website);
+    // two()
+    
+    
+}
+// one ()
+
+if (true){
+    const  username ="Kiran"
+    if (username==="Kiran"){
+        const website=" youtube"
+        // console.log(username + website );
+        
+    }
+    // console.log(website);
+    
+    
+}
+// console.log(username);
+
+console.log(addOne(5))
+
+function addOne(num){
+    return num + 1
+}
+
+addtwo(5)
+const addtwo =function(num){
+    return num + 2
+}
+
